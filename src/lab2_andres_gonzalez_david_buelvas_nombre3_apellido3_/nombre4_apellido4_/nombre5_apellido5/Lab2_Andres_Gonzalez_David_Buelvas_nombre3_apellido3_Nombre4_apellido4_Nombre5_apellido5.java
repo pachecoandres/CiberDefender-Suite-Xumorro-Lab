059136ab5003abb1e8731ffc869a15a8886892cc
Andres Gonzalez <pachecoandres@uninorte.edu.co.com>
2025-11-24 21:34:05 -0500

    primer repositorio

---
+++ src/lab2_andres_gonzalez_david_buelvas_nombre3_apellido3_/nombre4_apellido4_/nombre5_apellido5/Lab2_Andres_Gonzalez_David_Buelvas_nombre3_apellido3_Nombre4_apellido4_Nombre5_apellido5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2_andres_gonzalez_david_buelvas_nombre3_apellido3_.nombre4_apellido4_.nombre5_apellido5;
+
+/**
+ *
+ * @author andre
+ */
+public class Lab2_Andres_Gonzalez_David_Buelvas_nombre3_apellido3_Nombre4_apellido4_Nombre5_apellido5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu_principal frame = new Menu_principal();
+        frame.setVisible(true);
+    }
+    
+}
---
+++ src/lab2_andres_gonzalez_david_buelvas_nombre3_apellido3_/nombre4_apellido4_/nombre5_apellido5/PuntajesGlobales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2_andres_gonzalez_david_buelvas_nombre3_apellido3_.nombre4_apellido4_.nombre5_apellido5;
+
+/**
+ *
+ * @author andre
+ */
+public class PuntajesGlobales {
+    public static int modulo1 = 0;
+    public static int modulo2 = 0;
+    public static int modulo3 = 0;
+    public static int modulo4 = 0;
+}
